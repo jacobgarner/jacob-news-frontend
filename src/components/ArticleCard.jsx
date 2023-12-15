9cc5a526afa6a68d4f5a79ad6dc374a828d3ab6b
jacob <jacobabgarner@gmail.com>
2023-12-15 12:13:48 +0000

    adding sorting on article page

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,6 +1,6 @@
 
 export default function ArticleCard({article}) {
-  
+
   return (
     <div className="h-72 w-96 grid grid-cols-6 grid-rows-6 gap-0 text-left m-5 border-y rounded-md bg-slate-500 hover:bg-slate-700 cursor-pointer" >
         <h3 className="col-span-6 row-span-2 col-start-1 row-start-1 font-bold text-center">{article.title}</h3>
--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -6,14 +6,33 @@ import { useNavigate } from 'react-router-dom';
 export default function ArticlesList() {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true)
+  const [sortBy, setSortBy] = useState('')
+  const [sortOrder, setSortOrder] = useState('')
   const navigate = useNavigate();
 
   const queryParameters = new URLSearchParams(window.location.search)
 
   const topic = queryParameters.get("topic")
+
+  const compareArticles = (a, b) => {
+    console.log(a.comment_count)
+    switch (sortBy) {
+      case 'votes':
+        return sortOrder === 'asc' ? a.votes - b.votes : b.votes - a.votes;
+      case 'comment_count':
+        return sortOrder === 'asc' ? a.comment_count - b.comment_count : b.comment_count - a.comment_count;
+      case 'created_at':
+        return sortOrder === 'asc' ? new Date(a.created_at) - new Date(b.created_at) : new Date(b.created_at) - new Date(a.created_at);
+      default:
+        return 0;
+    }
+  };
+
     
 
-
+  const handleSortBy = (event) =>{
+    setSortBy(event.target.value)
+  }
 
   useEffect(() => {
     getArticles(topic).then((res) => {
@@ -22,18 +41,24 @@ export default function ArticlesList() {
     });
   }, [topic]);
 
+  const sortedArticles = [...articles].sort(compareArticles)
+
   if(isLoading) return <div><p>Loading...</p></div>
   
   return (
     <div>
-      <select defaultValue="Sort By">
+      <select value={sortBy} onChange={handleSortBy}>
         <option disabled>Sort By</option>
-        <option>cooking</option>
-        <option>coding</option>
-        <option>football</option>
+        <option value='created_at'>Date</option>
+        <option value='comment_count'>Comment count</option>
+        <option value='votes'>Votes</option>
       </select>
+      <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
       <ul className="flex flex-wrap">
-        {articles.map((article) => {
+        {sortedArticles.map((article) => {
           return (
             <li key={article.article_id} onClick={()=>{navigate(`/articles/${article.article_id}`)}} >
               {" "}
